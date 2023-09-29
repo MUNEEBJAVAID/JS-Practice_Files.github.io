f5518c19324436b5b4a7fdd60b1ca4c01afc1ec0
MUNEEBJAVAID <90055157+MUNEEBJAVAID@users.noreply.github.com>
2023-09-29 20:20:29 +0000

    Config done

---
+++ DynamicTyping.js
@@ -0,0 +1,14 @@
+/*
+ JS aek dynamic  typed langauge ha jis ma ham aek variable ma different type ke data ko store kar skty hen
+  let generic = "Muneeb";
+ console.log(generic);
+
+  generic = 5;
+ console.log(generic);
+
+  generic = false;
+ console.log(generic);
+
+ line no 3 par string , line 6 par number and line no 9 par boolean value de hoi ha aek e varaible ma.
+ */
+
