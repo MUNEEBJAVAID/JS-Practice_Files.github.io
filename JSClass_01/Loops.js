d67fe253bfdd559b98f76bf367a04c3baa88adb9
MUNEEBJAVAID <90055157+MUNEEBJAVAID@users.noreply.github.com>
2023-09-30 00:50:11 +0000

    ok

---
+++ JSClass_01/Loops.js
@@ -0,0 +1,44 @@
+/*
+Loops:
+jab kisi task ko bar-bar perform karna ho tu loop use karty hen
+
+print counting from 0 to 9:
+
+1: for loop
+
+for(initialization; condition; update) {
+    // loop ke andar chalane wale statements
+}
+
+for(let i = 0; i < 10; i++){
+    cout << i ;
+}
+
+2: While loop
+
+ while(condition) {
+    // loop ke andar chalane wale statements
+    // condition ko update karna na bhoolen, warna loop infinite ho sakta hai
+}
+
+let i= 0;
+
+while( i < 10){
+    cout << i;
+    i++;
+}
+
+3:do-while loop
+
+do {
+    // loop ke andar chalane wale statements
+} while(condition);
+
+let j= 0;
+do{
+    cout << j;
+    j++;
+}while( j < 10);
+
+*/
+
