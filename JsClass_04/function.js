5e6073600fef6c43b60dafb3423b6fb446d1d6d6
MUNEEBJAVAID <90055157+MUNEEBJAVAID@users.noreply.github.com>
2023-10-06 01:59:31 +0000

    ok

---
+++ JsClass_04/function.js
@@ -0,0 +1,16 @@
+// function declaration
+function funcName(){
+     console.log("Body of function");
+}
+
+// function call or invoke
+funcName();
+
+// function Assignment
+let invoke = function assign(){
+    console.log("Body of function");
+}
+
+console.log(invoke);
+invoke();
+.
