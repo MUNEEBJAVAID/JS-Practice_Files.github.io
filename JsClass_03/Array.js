568b5368a51437999f66ff60c14d801db683195c
MUNEEBJAVAID <90055157+MUNEEBJAVAID@users.noreply.github.com>
2023-10-06 00:25:23 +0000

    Js

--- JsClass_03/Array.js
+++ JsClass_03/Array.js
@@ -263,3 +263,17 @@ console.log(ans);
 
 
 
+const maxSpeed = {
+    car: 300, 
+    bike: 60, 
+    motorbike: 200, 
+    airplane: 1000,
+    helicopter: 400, 
+    rocket: 8 * 60 * 60
+};
+
+const sortable = Object.fromEntries(
+    Object.entries(maxSpeed).sort(([,a],[,b]) => a - b)
+);
+
+console.log(sortable);
